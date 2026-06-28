28564a4fbc855c0eaa3eb33850ab0afa5d2c4499
Balraj Singh <bytelifeofficial@gmail.com>
2026-06-28 12:14:57 +0530

    refactor: improve email validator through incremental validation rules

--- projects/authenticationSystem/app.js
+++ projects/authenticationSystem/app.js
@@ -16,3 +16,24 @@ function validateUsername(username){
     }
 }
 
+function validateEmail(email){
+    let cleanedEmail = email.trim()
+    let splitArray = cleanedEmail.split('@')
+
+    if(cleanedEmail.length === 0){
+        throw new ValidationError('email is required', 'email')
+    }
+    
+    if(splitArray.length < 2 || splitArray.length > 2){
+        throw new ValidationError('there should be one @', 'email')
+    }
+
+    if(splitArray[0].length === 0){
+        throw new ValidationError("Email must contain text before '@'.", 'email')
+    }
+
+    if(splitArray[1].length === 0){
+        throw new ValidationError("Email must contain text after '@'.", 'email')
+    }
+}
+
