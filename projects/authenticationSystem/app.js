015cc96ab6a11803f00c8542bb4ea5fc402b712c
Balraj Singh <bytelifeofficial@gmail.com>
2026-06-29 22:13:49 +0530

    feat: finalize email validation and architect password validation workflow

--- projects/authenticationSystem/app.js
+++ projects/authenticationSystem/app.js
@@ -18,22 +18,39 @@ function validateUsername(username){
 
 function validateEmail(email){
     let cleanedEmail = email.trim()
-    let splitArray = cleanedEmail.split('@')
+    let splitArrayAt = cleanedEmail.split('@')
+    let splitArrayDot = splitArrayAt[1].split('.')
 
     if(cleanedEmail.length === 0){
         throw new ValidationError('email is required', 'email')
     }
     
-    if(splitArray.length < 2 || splitArray.length > 2){
+    if(splitArrayAt.length < 2 || splitArrayAt.length > 2){
         throw new ValidationError('there should be one @', 'email')
     }
 
-    if(splitArray[0].length === 0){
+    if(splitArrayAt[0].length === 0){
         throw new ValidationError("Email must contain text before '@'.", 'email')
     }
 
-    if(splitArray[1].length === 0){
+    if(splitArrayAt[1].length === 0){
         throw new ValidationError("Email must contain text after '@'.", 'email')
     }
+
+    if(splitArrayDot.includes('')){
+        throw new ValidationError('There should not a space before or after a dot', "email")
+    }
+
+   
 }
+
+// validateEmail('abc@a.b.c.d..e.com')
+
+let arr = ['a',' b', 'c', 'd', 'e', 'com']
+
+console.log(arr[arr.length -1])
+
+let arr2 = ['a',' b', 'c', 'd', 'e']
+
+console.log(arr2[arr2.length -1])
 
