9d60e2e6a74b8b029763c9140caf79ae634448e5
Balraj Singh <bytelifeofficial@gmail.com>
2026-07-20 22:56:06 +0530

    docs: strengthen Promise.then mental model and refocus learning on roadmap essentials

--- javascriptCore/16_codingExercises/14_codeExercise.js
+++ javascriptCore/16_codingExercises/14_codeExercise.js
@@ -1,5 +1,14 @@
-function runAllProm(promArr) {
-  const promResults = {};
+function runAllProm(tasks) {
+  return new Promise((resolve, reject) => {
+    let counter = 0;
+    const promResults = [];
+
+    //for loop
+    for (let taskIndex = 0; taskIndex < tasks.length; taskIndex++) {
+      const prom = tasks[taskIndex]();
+      prom.then((prom) => {});
+    }
+  });
 }
 
 runAllProm([fethUser, fetchPosts, fetchTodos]);
